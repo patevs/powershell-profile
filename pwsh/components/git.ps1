35e405e325096ef173503ece99748ed1568df2eb
Patrick Evans <ppevans11@gmail.com>
2020-08-27 00:45:58 +1200

    :recycle: Moved posh-git config from console.ps1 to git.ps1

--- pwsh/components/git.ps1
+++ pwsh/components/git.ps1
@@ -6,6 +6,25 @@
 
 if ((Get-Command git -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable Posh-Git -ErrorAction SilentlyContinue)) {
   Import-Module -Name posh-git
+
+  # Posh-Git Configuration
+
+  # Remove 'posh~git' prefix from terminal title
+  # https://github.com/dahlbyk/posh-git/issues/424#issue-208094847
+  # TODO: Ensure posh-git is installed
+  $Global:GitPromptSettings.EnableWindowTitle = $true
+
+  # Dracula Prompt Configuration
+  # $GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+  # $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
+  # $GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
+  # $GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
+  # $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
+  # $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n > '
+  # Dracula Git Status Configuration
+  # $GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
+  # $GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
+  # $GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
 }
 
 # EOF #
